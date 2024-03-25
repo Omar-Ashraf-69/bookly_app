d86bdc48490e077b746040b00435da3730d2c51a
OmarAsharf62 <omarbondok62@gmail.com>
2024-03-25 06:46:58 +0200

    Add book.svg image, constants, and assets class
    - Added a new book.svg image for the app
    - Defined a primary color constant in constants.dart
    - Created an AssetsData class to manage app assets

--- lib/features/Splash/data/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/data/presentation/views/widgets/splash_view_body.dart
@@ -1,14 +1,32 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          
+          SvgPicture.asset(
+            AssetsData.logo,
+            height: 400,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Bookly',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
         ],
       ),
     );
