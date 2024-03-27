6ac5f952c9288fa138b9114cedf590dc501c8a2d
OmarAsharf62 <omarbondok62@gmail.com>
2024-03-27 04:56:03 +0200

    Refactor Failure class and handle DioException in HomeRepoImpl
    Refactored the Failure class to include error message and added handling
    of DioException in fetchBestSellerBooks method in HomeRepoImpl.

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,28 +3,32 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final ApiService apiService;
 
   HomeRepoImpl({required this.apiService});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async{
-
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
     //https://www.googleapis.com/books/v1/volumes?Sorting=newest&Filtering=free-ebooks&q=subject:cooking
     try {
-  var data = await apiService.get(
-      endpoint: 'volumes?Sorting=newest&Filtering=free-ebooks&q=subject:cooking');
+      var data = await apiService.get(
+          endpoint:
+              'volumes?Sorting=newest&Filtering=free-ebooks&q=subject:cooking');
 
-  List<BookModel> books = [];
-  for (var item in data['items']) {
-    books.add(BookModel.fromJson(item));
-  }
-  return right(books);
-
-}  catch (e) {
-  return left(ServerFailure());
-}
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(error: e.toString()));
+      }
+    }
   }
 
   @override
